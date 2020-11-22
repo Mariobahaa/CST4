c0eacc05cd79e485f43cf9f789c9ae58ad39c762
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2020-11-22 22:50:06 +0200

    Task 1 Completed

--- script1.js
+++ script1.js
@@ -1,14 +1,23 @@
-var str = window.prompt("Enter some text", "Hello World");
+
+var str;
+do{
+str = window.prompt("Enter some text", "Hello World");
+}while(str==null);
 var letter = window.prompt("Enter a letter to count", "a");
 var sensetive = window.confirm("Do you want to consider character casing?");
 
 var count = 0;
+var regex;
 
 if (sensetive)
 {
-    count = str.match(/letter/);
+    regex = new RegExp(letter,"g");
 }
 else
 {
-    count = str.match(/letter/i);
+     regex = new RegExp(letter,"ig");
 }
+count = (str.match(regex) || []).length;
+document.write(count);
+
+//Question: how to add a variable in regex without using new
